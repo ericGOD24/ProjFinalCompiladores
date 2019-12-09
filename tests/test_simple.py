3066fb717f17b50006466b98730975aaad88181c
ericGOD24 <ericobandeira@hotmail.com>
2019-12-09 13:39:23 -0200

    Tirando alguns testes

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -33,11 +33,12 @@ class TestRuntime:
         assert run('desejo uma tilapia 2 desce a conta chefia') == 7.38905609893065
         assert run('adiciona ao pedido 4 desejo uma batata frita desce a conta chefia') == 2
         assert run('adiciona ao pedido 10 campeão, da um desconto ai de 10 desce a conta chefia') == 1
+        
 
-    def test_eval_if_simple(self):
-        assert run('parcela ai em 0') == 'nao dá mano, so sorry'
-        assert run('parcela ai em -13') == 'ai tu quer né'
+    # def test_eval_if_simple(self):
+    #     assert run('parcela ai em 0') == 'nao dá mano, so sorry'
+    #     assert run('parcela ai em -13') == 'ai tu quer né'
 
-    def test_eval_if_nested(self):
-        assert run('retira da conta -1') == 'quer pagar pra estar aqui é?'
+    # def test_eval_if_nested(self):
+    #     assert run('retira da conta -1') == 'quer pagar pra estar aqui é?'
   
